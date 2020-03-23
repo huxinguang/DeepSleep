52466f02d324afab031e1b0d4e0c7dcaca45fb77
huxinguang <1259198268@qq.com>
2020-03-23 16:24:43 +0800

    fix

---
+++ DeepSleep/MainVC.swift
@@ -0,0 +1,45 @@
+//
+//  MainVC.swift
+//  DeepSleep
+//
+//  Created by xinguang hu on 2020/3/23.
+//  Copyright © 2020 wy. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Sunshine girl"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+    }
+    
+    @IBAction func onListBtn(_ sender: UIButton) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
